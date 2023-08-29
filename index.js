3b15f5e03149718b38632e4a2f6116dc77d3967b
Rohovska Nika <nikarogovska@gmail.com>
2023-08-29 10:20:01 +0300

    waw-service

---
+++ index.js
@@ -0,0 +1,118 @@
+const path = require('path');
+const template = path.join(process.cwd(), 'template');
+
+module.exports = async waw => {
+	waw.services = async (query, limit) => {
+		if (limit) {
+			return await waw.Service.find(query).limit(limit);
+		} else {
+			return await waw.Service.find(query);
+		}
+	}
+
+	waw.service = async (query) => {
+		return await waw.Service.findOne(query);
+	}
+
+	waw.crud('service', {
+		get: {
+			ensure: waw.next
+		},
+		fetch: {
+			ensure: waw.next,
+			query: req => {
+				return {
+					_id: req.body._id
+				}
+			}
+		}
+	})
+	const seo = {
+		title: waw.config.name,
+		description: waw.config.description,
+		image: 'https://body.webart.work/template/img/logo.png'
+	};
+
+	waw.build(template, 'services');
+	waw.build(template, 'service');
+	waw.serve_services = {};
+	waw.serve_service = {};
+	const services = async (req, res) => {
+		if (typeof waw.serve_services[req.get("host")] === "function") {
+			waw.serve_services[req.get("host")](req, res);
+		} else {
+			const tags = await waw.tags('service');
+
+			const services = await waw.services(
+				req.params.tag_id ?
+					{ tag: req.params.tag_id } :
+					{}
+			);
+			res.send(
+				waw.render(
+					path.join(template, 'dist', 'services.html'),
+					{
+						...seo,
+						description: 'Welcome to our platform, where you can find various services for your real estate needs. We offer a wide range of services that cover all aspects of construction, renovation, maintenance, and property management.',
+						services,
+						categories: await waw.tag_groups('service')
+					},
+					waw.translate(req)
+				)
+			)
+		}
+	}
+	waw.app.get('/services', services);
+	waw.app.get('/services/:tag_id', services);
+	waw.app.get('/service/:_id', async (req, res) => {
+		if (typeof waw.serve_service[req.get("host")] === "function") {
+			waw.serve_service[req.get("host")](req, res);
+		} else {
+			const tags = await waw.tags('service');
+
+			const service = await Service.findOne({
+				_id: req.params._id
+			});
+
+			res.send(
+				waw.render(
+					path.join(template, 'dist', 'service.html'),
+					{
+						...seo,
+						...{
+							service,
+							categories: await waw.tag_groups('service')
+						}
+					},
+					waw.translate(req)
+				)
+			)
+		}
+	});
+
+	const save_file = (doc) => {
+		if (doc.thumb) {
+			waw.save_file(doc.thumb);
+		}
+
+		if (doc.thumbs) {
+			for (const thumb of doc.thumbs) {
+				waw.save_file(thumb);
+			}
+		}
+	}
+
+	waw.on('service_create', save_file);
+	waw.on('service_update', save_file);
+	waw.on('service_delete', (doc) => {
+		if (doc.thumb) {
+			waw.delete_file(doc.thumb);
+		}
+
+		if (doc.thumbs) {
+			for (const thumb of doc.thumbs) {
+				waw.delete_file(thumb);
+			}
+		}
+	});
+};
---
+++ schema.js
@@ -0,0 +1,47 @@
+module.exports = function(waw) {
+	const Schema = waw.mongoose.Schema({
+		thumb: String,
+		thumbs: [String],
+		name: String,
+		short: String,
+		description: String,
+		data: {},
+		tag: {
+			type: waw.mongoose.Schema.Types.ObjectId,
+			ref: 'Tag'
+		},
+		author: {
+			type: waw.mongoose.Schema.Types.ObjectId,
+			ref: 'User'
+		},
+		moderators: [
+			{
+				type: waw.mongoose.Schema.Types.ObjectId,
+				sparse: true,
+				ref: 'User'
+			}
+		]
+	});
+
+	Schema.methods.create = function (obj, user, waw) {
+		this.author = user._id;
+
+		this.moderators = [user._id];
+
+		this.tag = obj.tag;
+
+		this.thumb = obj.thumb;
+
+		this.thumbs = obj.thumbs;
+
+		this.name = obj.name;
+
+		this.description = obj.description;
+
+		this.short = obj.short;
+
+		this.data = obj.data;
+	}
+
+	return waw.Service = waw.mongoose.model('Service', Schema);
+}
